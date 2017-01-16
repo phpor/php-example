ae4bc1b9ee348a32514d407f5c959d1907259ea2
李俊杰 <phpor@users.noreply.github.com>
2017-01-16 15:56:03 +0800

    Update sniffer.php

--- sniffer.php
+++ sniffer.php
@@ -6,6 +6,8 @@
     Needs root privileges , so use sudo !!
 */
 // sniffer with C : http://www.binarytides.com/packet-sniffer-code-in-c-using-linux-sockets-bsd-part-2/
+// 一般来讲，抓包都适用libcap，但是，这里没有，直接使用raw socket实现，显得非常的帅气和麻烦
+// 这里显示了php也能轻松实现很底层的工作（尽管是借助了socket模块），让你体验一下sniffer的原理，sniffer并不高深，php也能实现，只是效率就不要指望了
 
 error_reporting(~E_ALL);
 
